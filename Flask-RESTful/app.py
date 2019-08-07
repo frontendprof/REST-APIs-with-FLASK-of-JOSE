267ce920e7eb0180d25c845b9c7cfd1b9e4db9f0
Abdumalik Sharipov <abdumaliksharipov@gmail.com>
2019-08-07 18:13:14 -0500

    Some change

--- Flask-RESTful/app.py
+++ Flask-RESTful/app.py
@@ -5,13 +5,23 @@ from flask_restful import Resource, Api
 app=Flask(__name__)
 api=Api(app)
 
+items=[]
 
-class Student(Resource):
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name']==name:
+                return item
+
+
+    def post(self, name):
+        item {'name': name, 'price':13.00}
+        items.append()
+        return item
 
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
