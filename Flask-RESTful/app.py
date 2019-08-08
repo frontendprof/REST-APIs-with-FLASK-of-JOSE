9365a9ffeb54cef774b0002b03ffd1bd85d81341
Abdumalik Sharipov <abdumaliksharipov@gmail.com>
2019-08-08 01:24:22 -0500

    json web token library flask_jwt is used for authentiation purposes

--- Flask-RESTful/app.py
+++ Flask-RESTful/app.py
@@ -1,27 +1,43 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 
 app=Flask(__name__)
+app.secret_key='266567cd2f9c202764452b56e6093484'
 api=Api(app)
+
+
+jwt=JWT(app,authenticate,identity)
 
 items=[]
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name']==name:
-                return item
+        item =next(filter(lambda x:x['name']==name,items), None)
+        return {'item':item},200 if item else 404
 
 
     def post(self, name):
-        item {'name': name, 'price':13.00}
-        items.append()
-        return item
+        if next(filter(lambda x:x['name']==name,items), None):
+            return {'message': "An item with {} already exists".format(name)}, 400
+        data=request.get_json()
+        item ={'name': name, 'price':data['price']}
+        items.append(item)
+        return item, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
 
 
 
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
